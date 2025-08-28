a6e098b91f840eb689114f3f3eced60677dd197f
agiertli <agiertli@redhat.com>
2025-08-28 14:56:24 +0200

    v2

--- pipeline/link.sh
+++ pipeline/link.sh
@@ -1,3 +1,1 @@
-oc secrets link builder 1979710-ai-hackaton-pull-secret -n openshift-pipelines
-oc secrets link default 1979710-ai-hackaton-pull-secret --for=pull -n openshift-pipelines
 oc policy add-role-to-user system:image-puller system:serviceaccount:openshift-lightspeed:lightspeed-app-server -n openshift-pipelines
